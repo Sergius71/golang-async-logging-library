e4d4dd9d7c5d935febbf1322fd0f8ce6113844f3
Sergiy Podlubnij <sergiy.podlubnij@sandsiv.com>
2021-10-25 02:57:45 +0300

    Fixed WaitGroup type

--- alog.go
+++ alog.go
@@ -42,13 +42,13 @@ func New(w io.Writer) *Alog {
 // Start begins the message loop for the asynchronous logger. It should be initiated as a goroutine to prevent
 // the caller from being blocked.
 func (al Alog) Start() {
-	wg := sync.WaitGroup{}
+	wg := &sync.WaitGroup{}
 ex:
 	for {
 		select {
 		case message := <-al.msgCh:
 			wg.Add(1)
-			go al.write(message, &wg)
+			go al.write(message, wg)
 		case <-al.shutdownCh:
 			wg.Wait()
 			al.shutdown()
